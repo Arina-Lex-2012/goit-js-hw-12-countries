bce40040cf1bccfc03e23abc3e6223af7e921019
Maryna Berezhanska <arina.lex2012@gmail.com>
2021-05-19 08:47:23 +0300

    hbs

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import './sass/main.scss';
+// import menuItemTpl from './templates/menu-item.hbs';
+// import menuItems from './menu.json';
+
+fetch('https://restcountries.eu/rest/v2/name/usa')
+.then(response => {
+    return response.json();
+})
+.then(country => {
+    console.log(country);
+})
+.catch(error => {
+    console.log(error);
+});
+ 
+// https://restcountries.eu/rest/v2/name/{name}
+// https://restcountries.eu/rest/v2/name/eesti
+// https://restcountries.eu/rest/v2/name/united
+
+// name
+// capital: "Berlin"
+// population
+// languages
+// flag
