b78f40a2627a3eb95b162298e002391ab4824497
Maryna Berezhanska <arina.lex2012@gmail.com>
2021-05-21 09:29:14 +0300

    input.value

--- src/index.js
+++ src/index.js
@@ -3,23 +3,35 @@ import countryItemTpl from './templates/country-item.hbs';
 
 const refs = {
     cardContainer: document.querySelector('.js-card-container'),
+    searchInput: document.querySelector('.js-text-input'),
 };
 
-fetch('https://restcountries.eu/rest/v2/alpha/col')
-.then(response => {
-    return response.json();
-    
-})
-.then(country => {
-    console.log(country);
+refs.searchInput.addEventListener('input', onInput);
+
+function onInput(event) {
+    event.preventDefault();
+    const input = event.currentTarget;
+    const searchQuery = input.value;
+    // console.log(searchQuery);
+
+    fetchCountries(searchQuery)
+    .then(renderCountryCard)
+    .catch(error => console.log(error))
+    .finally(() => input.value = "");
+};
+
+
+// нахождение страны
+function fetchCountries(countryAlpha){
+    return fetch(`https://restcountries.eu/rest/v2/alpha/${countryAlpha}`)
+        .then(response => {
+            return response.json();
+        });
+};
+
+// рисование интерфейса 
+function renderCountryCard(country){
     const markup = countryItemTpl(country);
     console.log(markup);
-    refs.cardContainer.innerHTML = markup;
-})
-.catch(error => {
-    console.log(error);
-});
-
-function renderCountryCard(){
-
+    refs.cardContainer.innerHTML = markup;  
 };
